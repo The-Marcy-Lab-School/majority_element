9cb05cbb7b038ccea08dd1230e2ea79a3d8efa53
EC2 Default User <ec2-user@ip-172-31-82-78.ec2.internal>
2020-01-09 15:16:27 +0000

    updated

--- majority_element.py
+++ majority_element.py
@@ -1,13 +1,11 @@
 def majorityEl(arr):
     totalEl = {}
-    big = 0
-    for el in totalEl:
+    for el in arr:
         if el in totalEl:
             totalEl[el] += 1
         else:
             totalEl[el] = 1
-        if totalEl[el] > big:
-            big = totalEl[el]
+
+    for el in totalEl:
+        if totalEl[el] > len(arr)/2:
             return el
-            
-majorityEl([3,2,3])
